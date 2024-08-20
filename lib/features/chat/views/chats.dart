4bb0ac6cfad100464e1d8a320b61e1d6400e8138
Captured-Heart <knkpozi@gmail.com>
2024-08-20 12:25:17 +0100

    updated the image viewer to cache network image

--- lib/features/chat/views/chats.dart
+++ lib/features/chat/views/chats.dart
@@ -19,7 +19,7 @@ import '../../../router.dart';
 import '../../subscription/provider/subscription_provider.dart';
 import 'chat_rep.dart';
 
-class ChatsScreen extends StatefulWidget  {
+class ChatsScreen extends StatefulWidget {
   const ChatsScreen({super.key});
 
   @override
@@ -28,7 +28,6 @@ class ChatsScreen extends StatefulWidget  {
 
 class _ChatsScreenState extends State<ChatsScreen> with WidgetsBindingObserver {
   var ctr = Get.put(ChatCtr());
-
 
   @override
   Widget build(BuildContext context) {
@@ -50,92 +49,91 @@ class _ChatsScreenState extends State<ChatsScreen> with WidgetsBindingObserver {
         ],
       ),
       body: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 15.sp, vertical: 15.sp),
-                child: Obx(
-                  () => Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Recent Matches',
-                        style: textStyle18.copyWith(
-                          fontWeight: FontWeight.w800,
-                        ),
-                      ),
-                      const SizedBoxH15(),
-                      ctr.allChatUsers.isNotEmpty
-                          ? SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        physics: const BouncingScrollPhysics(),
-                        child: Row(
-                          children: [
-                            ...ctr.allChatUsers.map((users) {
-                              return GestureDetector(
-                                onTap: () async {
-                                  Get.to(() => ChatWithScreen(chatModel: users));
-
-                                },
-                                child: Padding(
-                                  padding: const EdgeInsets.all(12.0),
-                                  child: CircleAvatar(
-                                    radius: 32,
-                                    backgroundImage: NetworkImage(
-                                        users.userModel!.photos![0]),
-                                  ),
-                                ),
-                              );
-                            }).toList()
-                          ],
-                        ),
-                      )
-                          : Padding(
-                              padding: const EdgeInsets.only(top: 24.0),
-                              child: Center(
-                                  child: Text(
-                                "You don’t have any matches yet",
-                                style: textStyle14.copyWith(color: dustyGrey),
-                              )),
-                            ),
-                      const SizedBoxH40(),
-                      Text('Chats', style: textStyle18.copyWith(fontWeight: FontWeight.w800)),
-                      const SizedBoxH10(),
-                      ctr.allChatUsers.where((val) => val.lastMessage.isNotEmpty).isNotEmpty
-                          ? ListView(
-                              shrinkWrap: true,
-                              children: [
-                                ...ctr.allChatUsers
-                                    .where((val) => val.lastMessage.isNotEmpty)
-                                    .map((val) {
-                                  return ChatContainer(
-                                    image: val.userModel!.photos![0],
-                                    name: val.userModel!.username,
-                                    time: val.timestamp.toDate(),
-                                    text: val.lastMessage,
-                                    count: ctr.auth.currentUser!.uid == val.userSentLastMessage
-                                        ? 0
-                                        : val.unreadCount,
-                                    onPress: () {
-                                      Get.to(() => ChatWithScreen(chatModel: val));
-                                    },
-                                  );
-                                }).toList(),
-                              ],
-                            )
-                          : SizedBox(
-                              height: Get.height / 2,
-                              child: const Center(
-                                child: EmptyStateWidget(
-                                    shouldShowImage: false,
-                                    message: 'You will see your chats here'),
-                              ),
-                            )
-                    ],
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15.sp, vertical: 15.sp),
+            child: Obx(
+              () => Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Recent Matches',
+                    style: textStyle18.copyWith(
+                      fontWeight: FontWeight.w800,
+                    ),
                   ),
-                ),
+                  const SizedBoxH15(),
+                  ctr.allChatUsers.isNotEmpty
+                      ? SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          physics: const BouncingScrollPhysics(),
+                          child: Row(
+                            children: [
+                              ...ctr.allChatUsers.map((users) {
+                                return GestureDetector(
+                                  onTap: () async {
+                                    Get.to(() => ChatWithScreen(chatModel: users));
+                                  },
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(12.0),
+                                    child: CacheNetworkWidget(
+                                      imgUrl: users.userModel!.photos![0],
+                                      height: 64,
+                                      width: 64,
+                                      isNotCircle: false,
+                                    ),
+                                  ),
+                                );
+                              }).toList()
+                            ],
+                          ),
+                        )
+                      : Padding(
+                          padding: const EdgeInsets.only(top: 24.0),
+                          child: Center(
+                              child: Text(
+                            "You don’t have any matches yet",
+                            style: textStyle14.copyWith(color: dustyGrey),
+                          )),
+                        ),
+                  const SizedBoxH40(),
+                  Text('Chats', style: textStyle18.copyWith(fontWeight: FontWeight.w800)),
+                  const SizedBoxH10(),
+                  ctr.allChatUsers.where((val) => val.lastMessage.isNotEmpty).isNotEmpty
+                      ? ListView(
+                          shrinkWrap: true,
+                          children: [
+                            ...ctr.allChatUsers
+                                .where((val) => val.lastMessage.isNotEmpty)
+                                .map((val) {
+                              return ChatContainer(
+                                image: val.userModel!.photos![0],
+                                name: val.userModel!.username,
+                                time: val.timestamp.toDate(),
+                                text: val.lastMessage,
+                                count: ctr.auth.currentUser!.uid == val.userSentLastMessage
+                                    ? 0
+                                    : val.unreadCount,
+                                onPress: () {
+                                  Get.to(() => ChatWithScreen(chatModel: val));
+                                },
+                              );
+                            }).toList(),
+                          ],
+                        )
+                      : SizedBox(
+                          height: Get.height / 2,
+                          child: const Center(
+                            child: EmptyStateWidget(
+                                shouldShowImage: false, message: 'You will see your chats here'),
+                          ),
+                        )
+                ],
               ),
-            ],
+            ),
           ),
+        ],
+      ),
     );
   }
 
