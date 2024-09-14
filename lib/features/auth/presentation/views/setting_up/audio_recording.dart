c0731b3dd2cd906ab0df4f17fef88184b1fb7d0e
Nexus4Singles <nexusgodlydatingapp@gmail.com>
2024-09-14 14:49:41 +0100

    Update audio_recording.dart

--- lib/features/auth/presentation/views/setting_up/audio_recording.dart
+++ lib/features/auth/presentation/views/setting_up/audio_recording.dart
@@ -74,8 +74,8 @@ class _AudioRecordingScreenState extends State<AudioRecordingScreen> {
                       ),
                       TextSpan(
                         text:
-                            '''\nEach response has a limit of 60 seconds and you will not be able to change your responses after your profile is completed. \nYour responses don´t need to be perfect, they just need to be audible & authentic. \nRemember that people value authenticity and most people can tell when a response feels rehearsed or scripted, so we recommend reflecting deeply on each question & responding from your heart, to avoid wondering why you’re not getting matches, despite saying impressive things in your responses. 
-It is also obvious that any user who records gibberish or submits empty recordings will not be taken seriously by other users, and such profiles will be deleted. \n
+                            '''\nEach response has a limit of 60 seconds and you will not be able to change your responses after your profile is completed. \n\nYour responses don´t need to be perfect, they just need to be audible & authentic. \n\nRemember that people value authenticity and most people can tell when a response feels rehearsed or scripted, so we recommend reflecting deeply on each question & responding from your heart, to avoid wondering why you’re not getting matches, despite saying impressive things in your responses. 
+It is also obvious that any user who records gibberish or submits empty recordings will not be taken seriously by other users, and such profiles will be deleted. \n\n Lastly, please avoid background noises so that users can hear you clearly.\n
 Happy Recording!''',
                       ),
                     ],
