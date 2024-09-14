872b4217f40fc020bd7e38185c01ead13b19700d
Nexus4Singles <nexusgodlydatingapp@gmail.com>
2024-09-14 08:39:03 +0100

    Update age.dart

--- lib/features/auth/presentation/views/setting_up/age.dart
+++ lib/features/auth/presentation/views/setting_up/age.dart
@@ -70,7 +70,7 @@ class _AgeScreenState extends State<AgeScreen> {
                         color: black),
                   ),
                   Text(
-                    "Nexus is for users between the ages of 21 to 70 years \n Kindly note that you won't be able to change this later",
+                    "Nexus is for users between the ages of 21 to 70 years.\n Kindly note that you won't be able to change this later.\n You will have the option of hiding your age from users soon.",
                     style: textStyle14,
                     textAlign: TextAlign.center,
                   ),
