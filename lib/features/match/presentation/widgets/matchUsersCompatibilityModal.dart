cc16ecb3de3a41f1a87d7728e854d6fdb9920176
Nexus4Singles <nexusgodlydatingapp@gmail.com>
2024-09-18 23:22:46 +0100

    Update matchUsersCompatibilityModal.dart

--- lib/features/match/presentation/widgets/matchUsersCompatibilityModal.dart
+++ lib/features/match/presentation/widgets/matchUsersCompatibilityModal.dart
@@ -74,10 +74,10 @@ class MatchedUsersCompatibilityModal extends StatelessWidget {
                     "${userModel.username} ${userModel.compatibility!.believeInCohiabiting!.toLowerCase().contains("yes") ? "believes" : "doesn't believe"} in cohabiting before marriage",
                   ),
                   optionsMenu(
-                    "${userModel.username} ${userModel.compatibility!.shouldChristianSpeakInTongue!.toLowerCase().contains("yes") ? "believes" : "doesn't believe"} in speaking in tongues",
+                    "${userModel.username} ${userModel.compatibility!.shouldChristianSpeakInTongue!.toLowerCase().contains("yes") ? "believes" : "doesn't believe"} speaking in tongues is very necessary",
                   ),
                   optionsMenu(
-                    "${userModel.username} ${userModel.compatibility!.believeInTithing!.toLowerCase().contains("yes") ? "believes" : "doesn't believe"} in tithing",
+                    "${userModel.username} ${userModel.compatibility!.believeInTithing!.toLowerCase().contains("yes") ? "believes" : "doesn't believe"} tithing is important",
                   )
                 ],
               ),
