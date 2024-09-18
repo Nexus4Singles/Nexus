10d5a0f112bf0a41d3fa3e839d7227e32a6809c3
Nexus4Singles <nexusgodlydatingapp@gmail.com>
2024-09-18 23:26:35 +0100

    Update home.dart

--- lib/features/home/presentation/views/home.dart
+++ lib/features/home/presentation/views/home.dart
@@ -139,7 +139,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                             model.jumpToNavPage(1);
                                           },
                                           message:
-                                              "Check Back Tomorrow or Use the Explore Page to Search & Filter Profiles Within Other Countries",
+                                              "Check Back Tomorrow or Use the Explore Page to Search & Filter Profiles Within Any Country",
                                         );
                                       }),
                                       //TODO: USE THIS COUNTER TO DISPLAY THE REMAINING TIME FOR USERS TO SEE THE RECOMMENDED LIST
