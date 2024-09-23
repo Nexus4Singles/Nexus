1332368464fb916095498ed7567ee38fb00049c3
Nexus4Singles <nexusgodlydatingapp@gmail.com>
2024-09-23 19:58:42 +0100

    Update matchUsersCompatibilityModal.dart

--- lib/features/match/presentation/widgets/matchUsersCompatibilityModal.dart
+++ lib/features/match/presentation/widgets/matchUsersCompatibilityModal.dart
@@ -77,7 +77,7 @@ class MatchedUsersCompatibilityModal extends StatelessWidget {
                     "${userModel.username} ${userModel.compatibility!.shouldChristianSpeakInTongue!.toLowerCase().contains("yes") ? "believes" : "doesn't believe"} speaking in tongues is very necessary",
                   ),
                   optionsMenu(
-                    "${userModel.username} ${userModel.compatibility!.believeInTithing!.toLowerCase().contains("yes") ? "believes" : "doesn't believe"} tithing is important",
+                    "${userModel.username} ${userModel.compatibility!.believeInTithing!.toLowerCase().contains("yes") ? "thinks" : "doesn't think"} tithing is so important",
                   )
                 ],
               ),
