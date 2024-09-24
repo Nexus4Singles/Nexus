11cfa959da5f6ccb3bca344afe46c743ab723a76
Nexus4Singles <nexusgodlydatingapp@gmail.com>
2024-09-24 06:56:35 +0100

    Update report_user.dart

--- lib/features/profile/presentation/views/report_user.dart
+++ lib/features/profile/presentation/views/report_user.dart
@@ -37,7 +37,7 @@ class ReportUser extends StatelessWidget {
               child: Text(
                   "We want to ensure the quality of users on Nexus are constantly refined. Please assist us to achieve this by reporting any user who was toxic to you, or any user with inappropriate pictures or recordings. Thank you!",
                   textAlign: TextAlign.center,
-                  style: textStyle12),
+                  style: textStyle14),
             ),
             const SizedBoxH25(),
             CustomTextField(
