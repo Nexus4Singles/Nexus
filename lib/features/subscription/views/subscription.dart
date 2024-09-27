e068d30a6c3980f166fd15065eab7aa01b82f1e0
Nexus4Singles <nexusgodlydatingapp@gmail.com>
2024-09-27 01:24:46 +0100

    Update subscription.dart

--- lib/features/subscription/views/subscription.dart
+++ lib/features/subscription/views/subscription.dart
@@ -51,7 +51,7 @@ class _SubscriptionScreenState extends State<SubscriptionScreen> {
             children: [
               SizedBox(height: 10.h),
               Text(
-                'The free version of Nexus provides unlimited access to most features. However, some features have been made premium. We incur some monthly costs to ensure your profiles and audio recordings are securely stored and accessible to users. Hence, we are charging a small subscription fee to access premium features. This is to ensure we are able to maintain our services to you.\n If you live in Nigeria, please note that your naira debit cards will not work for subscriptions. Kindly use a dollar card or virtual card to make payment. Using a Grey virtual card or Chipper Cash is recommended for this. We apologize for this inconvenience as this is due to Nigerian bank restrictions for in-app payments',
+                'The free version of Nexus provides unlimited access to most features. However, some features have been made premium. We incur some monthly costs to ensure your profiles and audio recordings are securely stored and accessible to users. Hence, we are charging a small subscription fee to access premium features. This is to ensure we are able to maintain our services to you.\n\nIf you live in Nigeria, please note that your naira debit cards will not work for subscriptions. Kindly use a dollar card or virtual card to make payment. Using a Grey virtual card or Chipper Cash is recommended for this. We apologize for this inconvenience as this is due to Nigerian bank restrictions for in-app payments',
                 style: textStyle14.copyWith(
                   fontSize: 11.sp,
                   color: black,
