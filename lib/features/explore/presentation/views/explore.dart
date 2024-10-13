09f2047471ba843344dffcf6074f0690696bd5b7
Nexus4Singles <nexusgodlydatingapp@gmail.com>
2024-10-13 14:02:03 +0100

    Update explore.dart

--- lib/features/explore/presentation/views/explore.dart
+++ lib/features/explore/presentation/views/explore.dart
@@ -159,7 +159,7 @@ class _ExploreScreenState extends State<ExploreScreen> {
                               height: Get.height / 1.5,
                               child: Center(
                                 child: Text(
-                                  'Stay in control of profiles you see!\n Search for profiles within any country and get more desired results using filters',
+                                  'Stay in control of users you see by exploring profiles across the world & using filters to get more desired results ',
                                   style: textStyle14.copyWith(
                                     color: dustyGrey,
                                   ),
