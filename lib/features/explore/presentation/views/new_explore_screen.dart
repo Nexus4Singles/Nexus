174ed7569c4dee0cc5dc99fe6932217e6cf56cef
Nexus4Singles <nexusgodlydatingapp@gmail.com>
2024-10-29 10:01:35 +0100

    Update new_explore_screen.dart

--- lib/features/explore/presentation/views/new_explore_screen.dart
+++ lib/features/explore/presentation/views/new_explore_screen.dart
@@ -53,7 +53,7 @@ class _NewExploreScreenState extends State<NewExploreScreen> {
       } else {
         restrictionModal(
           text:
-              'This is a premium feature. The home page recommends 10 profiles to you per day but the Explore page helps you to narrow down your search using advanced filters.',
+              'This is a premium feature\n. The home page recommends 10 profiles to you daily but you can speed up your search by using advanced filters on this page.',
           context: context,
         );
 
