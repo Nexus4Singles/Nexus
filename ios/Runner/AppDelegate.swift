7a773dde5355eb7ba75efdfed5916808a4e7a133
Ayoola <ayo.oloyede16@gmail.com>
2024-11-02 06:36:30 +0100

    Bug fix

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -4,12 +4,15 @@ import FirebaseCore
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
+  lazy var flutterEngine = FlutterEngine(name: "myFlutterEngine")
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
       FirebaseApp.configure()
-    GeneratedPluginRegistrant.register(with: self)
+      flutterEngine.run()
+
+    GeneratedPluginRegistrant.register(with: flutterEngine)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
