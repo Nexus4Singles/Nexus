c19266024a8d6a99fc36eadafc9faee3693f72eb
Nexus4Singles <nexusgodlydatingapp@gmail.com>
2025-08-08 16:48:04 +0100

    /android/build.gradle

--- android/app/src/main/kotlin/com/example/nexus/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/nexus/MainActivity.kt
@@ -5,37 +5,31 @@ import android.app.NotificationManager
 import android.media.RingtoneManager
 import android.net.Uri
 import android.os.Build
-import io.flutter.embedding.android.FlutterActivity
 import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-    
+class MainActivity : FlutterActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        createNotificationChannel() // Call the method to create the notification channel
+        createNotificationChannel()
     }
 
     private fun createNotificationChannel() {
-        // This will only run on Android 8.0 (API level 26) and above
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channelId = "NEXUS"
             val channelName = "Nexus Notifications"
             val channelDescription = "Channel for Nexus app notifications"
-            
-            // Create a sound URI for default notification sound
             val soundUri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-
-            // Set the importance of the channel
             val importance = NotificationManager.IMPORTANCE_HIGH
 
-            // Create the NotificationChannel
             val channel = NotificationChannel(channelId, channelName, importance).apply {
                 description = channelDescription
-                setSound(soundUri, null) // Set the sound for this channel
+                setSound(soundUri, null)
             }
 
-            // Register the channel with the system
-            val notificationManager: NotificationManager = getSystemService(NotificationManager::class.java)
+            val notificationManager: NotificationManager =
+                getSystemService(NotificationManager::class.java)
             notificationManager.createNotificationChannel(channel)
         }
     }
